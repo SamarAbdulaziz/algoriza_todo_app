f573151692ab528e078deaab5778791c42ccd304
Samar <sam_abdulaziz@yahoo.com>
2022-08-02 03:59:03 +0200

    favorite issue fixed

--- lib/features/board/presentation/widgets/all_tasks.dart
+++ lib/features/board/presentation/widgets/all_tasks.dart
@@ -2,8 +2,6 @@ import 'package:algoriza_todo_app/core/todo_app_cubit/todo_cubit.dart';
 import 'package:algoriza_todo_app/core/util/widgets/task_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
-
 class AllTasksSection extends StatelessWidget {
   const AllTasksSection({Key? key}) : super(key: key);
 
@@ -17,14 +15,14 @@ class AllTasksSection extends StatelessWidget {
       builder: (context, state) {
         return ListView.builder(
           itemBuilder: (context, index) {
-            DateTime date =
-            DateFormat.jm().parse(cubit.allTasks[index]['startTime']);
-            var myTime = DateFormat("HH:mm").format(date);
-            cubit.notification.scheduledNotification(
-              int.parse(myTime.toString().split(":")[0]),
-              int.parse(myTime.toString().split(":")[1]),
-              cubit.allTasks[index],
-            );
+            // DateTime date =
+            // DateFormat.jm().parse(cubit.allTasks[index]['startTime']);
+            // var myTime = DateFormat("HH:mm").format(date);
+            // cubit.notification.scheduledNotification(
+            //   int.parse(myTime.toString().split(":")[0]),
+            //   int.parse(myTime.toString().split(":")[1]),
+            //   cubit.allTasks[index],
+            // );
             return TaskItem(
               taskItem: cubit.allTasks[index],
             );
